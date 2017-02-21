b0074c2ba7338320b7f82333d5fe6056bcec558f
Richard Sipes <rksipes@gmail.com>
2017-02-20 16:50:10 -0800

    tutorial complete

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -1,5 +1,29 @@
+Template.Recipe.onCreated(function() {
+    // Set a variable scoped to this template
+    this.editMode = new ReactiveVar(false);
+});
+
+Template.Recipe.helpers({
+    // returns the current recipe's id
+    updateRecipeId: function() {
+        return this._id;
+    },
+    editMode: function() {
+        // return the variable scoped to this template
+        return Template.instance().editMode.get();
+    }
+})
+
 Template.Recipe.events({
     'click .toggle-menu': function() {
         Meteor.call('toggleMenuItem', this._id, this.inMenu);
+    },
+    'click .fa-trash': function() {
+        Meteor.call('deleteRecipe', this._id);
+    },
+    'click .fa-pencil': function(event, template) {
+        template.editMode.set(!template.editMode.get());
+        // Default session value is false.
+        // Session.set('editMode', !Session.get('editMode'));
     }
 })
